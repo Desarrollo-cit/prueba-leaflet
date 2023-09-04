481c21a6d90d587bad869f394b3f474e55f91f69
calo13 <carlologo96@gmail.com>
2023-09-04 09:36:54 -0600

    fin del ejemplo

---
+++ views/mapa/index.php
@@ -0,0 +1,5 @@
+<h1 class="text-center">Mapas leaflet</h1>
+<div class="row justify-content-center">
+    <div class="col-lg-11 border rounded" id="mapa" style="height: 60vh; min-height:auto;"></div>
+</div>
+<script src="<?= asset('./build/js/mapa/index.js') ?>"></script>
